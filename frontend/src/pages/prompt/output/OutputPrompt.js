5443b1a3ac6a6607659904e296fc31110e19121f
ktony <k.tony9412@gmail.com>
2025-02-17 13:14:01 -0500

    chore OutputPrompt update
    update OutputPrompt to show output values from backend to users
    
    its design also completely changed

--- frontend/src/pages/prompt/output/OutputPrompt.js
+++ frontend/src/pages/prompt/output/OutputPrompt.js
@@ -1,9 +1,27 @@
-import React from "react";
+import { React, useEffect, useRef } from "react";
+import "./OutputPrompt.scss";
 
 function OutputPrompt({ outputPrompt }) {
+  const outputTextareaRef = useRef(null);
+
+  useEffect(() => {
+    if (outputTextareaRef.current) {
+      outputTextareaRef.current.style.height = "auto";
+      outputTextareaRef.current.style.height = `${outputTextareaRef.current.scrollHeight}px`;
+    }
+  }, [outputPrompt]);
+
   return (
-    <div>
-      <textarea value={outputPrompt} readOnly />
+    <div className="output-container">
+      <div className="output-box">
+        <textarea
+          ref={outputTextareaRef}
+          className="output-textarea"
+          value={outputPrompt}
+          readOnly
+          placeholder="The answer will appear here!"
+        />
+      </div>
     </div>
   );
 }
