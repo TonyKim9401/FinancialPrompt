8acbbaa81d3ce7ac800dc3e14ef16a0c72b463a5
ktony <k.tony9412@gmail.com>
2025-02-18 10:19:21 -0500

    test App test code
    add App.js main test code

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App.js";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Component", () => {
+  it("renders main title", () => {
+    render(<App />);
+    expect(
+      screen.getByText("FinChat Assignment by Tony Kim")
+    ).toBeInTheDocument();
+  });
 });
